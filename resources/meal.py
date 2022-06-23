4981ee982b3d11b5d67f3dfe4d357f0fd2564d1b
Daniel Díaz <me@danieldiaz.me>
2022-06-22 21:18:54 -0500

    added an error message when meal not found

--- resources/meal.py
+++ resources/meal.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 from db.db import get_db
 from db.requests.meal import get_all_meal
@@ -8,9 +8,8 @@ from db.requests.meal import get_all_meal
 router = APIRouter(prefix='/menu', tags=['menu'])
 
 @router.get('/{date}/{type_}')
-def get_all_meals(date:str, type_:str, response:Response, db:Session=Depends(get_db)):
+def get_all_meals(date:str, type_:str, db:Session=Depends(get_db)):
     meal_names = get_all_meal(db, date, type_)
     if len(meal_names) < 1:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "There is not meal that match the criteria.")
     return [meal.name for meal in meal_names]
